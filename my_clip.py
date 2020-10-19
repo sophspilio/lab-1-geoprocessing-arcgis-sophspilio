67c50114ceb40f93e49d631f3e9c1704f18a0dc8
sophspilio <70207777+sophspilio@users.noreply.github.com>
2020-10-19 10:26:56 -0400

    Update my_clip.py

--- my_clip.py
+++ my_clip.py
@@ -1,5 +1,16 @@
-# Name: YOUR NAME HERE 
-# Python Version: ENTER ASSIGNMENT TITLE HERE.
-# Description: ENTER A DESCRIIPTION ABOUT WHAT YOUR CODE IS SUPPOSED TO DO HERE: DESCRIBE INPUTS / OUTPUTS.
+# Name: Sophie Spiliotopoulos
+# Python Version: Python 2.7
+# Description: Code Lab 1- a script to clip the lakes.shp based on the basin.shp file, to create a new layer that shows the lakes within the basin. 
 
-Leave the lines of comment code, above, but add your personal details. Then replace these two lines of code with your new code! 
+import arcpy
+
+#set environmnet
+arcpy.env.workspace = "E:\\Comp Programming for GIS\\Data_Lab_1_Geoprocessing_ArcGIS"
+
+#set local variables
+lakes = "lakes.shp"
+basin = "basin.shp"
+lakes_myClip_shp = "E:\\Comp Programming for GIS\\Data_Lab_1_Geoprocessing_ArcGIS\\Results\\lakes_myClip.shp"
+
+#process clip 
+arcpy.Clip_analysis(lakes , basin ,lakes_myClip_shp, "")
